6ffd8a84b51a1f6ead21e8c0ff2796580b22cc51
Alex Dauncey <84759985+Shylexx@users.noreply.github.com>
2022-11-22 11:31:42 +0000

    breakpoint exception interrupts

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,17 @@ use shy_os::println;
 // ENTRY POINT
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World!, Here are some numbers: {}, {}", 420, 69 / 3);
+    println!("Hello World!");
+
+    shy_os::init();
+
+    // Breakpoint exception
+    x86_64::instructions::interrupts::int3();
 
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     loop {}
 }
 
